1a652bb880394249d7162da3e047996354e99345
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-06-11 20:40:33 -0400

    Improve data parser code style

--- data_parser.py
+++ data_parser.py
@@ -10,105 +10,192 @@ each trajectory) with the following keys:
     - file_path: the path to the trajectory file
     - class: the class of the trajectory
 """
-import os, numpy as np, datetime as dt, re, json
+import json
+import logging
+from datetime import datetime
 from pathlib import Path
+from typing import List, NamedTuple, Tuple
 
-root = Path('./geolife_dataset')
-metadata = []
+import numpy as np
 
-def trajectories_usr(usr: Path):
-    if usr.joinpath('labels.txt').exists():
-        labels = parse_labels(open(usr.joinpath('labels.txt'),'r').read())
-        trajs = parse_an_user_traj(usr)
-        try:
-            os.mkdir('./trajectories/'+usr.name)
-        except:
-            pass
-        index = 0
-        i = 0
-        dt0 = labels[index][0]
-        dte = labels[index][1]
-        traj = []
-        while i < len(trajs):
-            reg_date = parse_a_date(trajs[i][2])
-            reg_time = parse_a_time(trajs[i][3])
-            reg_datetime = create_datetime(reg_date, reg_time)
-            if dt0 <= reg_datetime <= dte:
-                traj.append([trajs[i][0], trajs[i][1], range_time(dt0, reg_datetime)])
-            elif dte < reg_datetime:
-                i-=1
-                if len(traj) > 0:
-                    traj = np.array(traj)
-                    class_ = labels[index][2]
-                    ID = usr.name +'_'+ str(index)
-                    file_path = './trajectories/'+usr.name+'/'+str(index)+'_'+ class_
-                    np.savetxt(file_path, traj)
-                    metadata.append({'id': ID, 'file_path': file_path, 'class': class_})
-                    traj = []
-                index += 1
-                if index >= len(labels): 
-                    return 0
-                dt0 = labels[index][0]
-                dte = labels[index][1]
-            i+=1
-    return -1
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
+)
 
-def parse_an_user_traj(usr: Path) -> list:
-    trajs_path = usr.joinpath('Trajectory')
-    data = []
-    sorted_paths = sorted([i for i in trajs_path.iterdir()])
-    for plt in sorted_paths:
-        with open(plt, 'r') as reg:
-            data += parse_a_plt(reg.read())
-    return data
-    
-def parse_a_plt(plt: str) -> list:
-    lines = plt.splitlines()[6:]
-    reg_plt = []
-    for line in lines:
-        reg_plt.append(parse_a_register(line))
-    return reg_plt
+metadata: List[dict] = []
 
-def parse_labels(labels: str) -> list:
-    clfs = []
-    labels = labels.splitlines()[1:]
-    for line in labels:
-        clfs.append(parse_label(line))
-    return clfs
 
-def parse_label(line: str) -> list:
-    line = line.split()
-    ds, ts = parse_a_date(line[0]), parse_a_time(line[1])
-    de, te = parse_a_date(line[2]), parse_a_time(line[3])
-    sdt = create_datetime(ds, ts)
-    edt = create_datetime(de, te)
-    return [sdt, edt, line[4]]
+LabelData = NamedTuple(
+    "LabelData", [("start_dt", datetime), ("end_dt", datetime), ("clsf", str)]
+)
 
-def parse_a_register(register: str) -> list:
-    reg = register.split(',')
+
+Register = Tuple[float, float, datetime]
+"""Register data (GPS point and time) in the form: lat, lon, time"""
+
+
+def process_usr_trajs(usr_folder: Path) -> None:
+    """
+    Processes the trajectories of a user.
+
+    Parameters
+    ----------
+    usr_folder : Path
+        The path to the user folder.
+    """
+    labels_file = usr_folder / "labels.txt"
+    if not labels_file.exists():
+        return
+
+    labels = load_labels(labels_file)
+    all_regs = load_registers(usr_folder)
+
+    logging.info("Processing trajectories")
+    dest_folder = Path(f"./trajectories/{usr_folder.name}")
+    dest_folder.mkdir(parents=True, exist_ok=True)
+
+    label_idx = 0
+    regs_idx = 0
+    label = labels[label_idx]
+    traj = []
+    while regs_idx < len(all_regs):
+        print(f"{(regs_idx + 1) / len(all_regs):.2%}", end="\r")
+        lat, long, reg_dt = all_regs[regs_idx]
+
+        # Register inside label bounds
+        if label.start_dt <= reg_dt <= label.end_dt:
+            traj.append([lat, long, (reg_dt - label.start_dt).seconds])
+
+        # Register outside label bounds
+        elif label.start_dt < reg_dt:
+            # Save the trajectory if it is not empty
+            if traj:
+                traj_id = f"{usr_folder.name}_{label_idx}"
+                file_path = str(dest_folder / f"{label_idx}_{label.clsf}.txt")
+                np.savetxt(file_path, np.array(traj))
+                metadata.append(
+                    {"id": traj_id, "file_path": file_path, "class": label.clsf}
+                )
+                traj = []
+
+            # Move to the next label
+            label_idx += 1
+            if label_idx >= len(labels):
+                break
+            label = labels[label_idx]
+            continue  # Don't increment regs_idx yet
+        regs_idx += 1
+
+
+def load_registers(usr_folder: Path) -> List[Register]:
+    """
+    Loads all the registers of a user.
+
+    Parameters
+    ----------
+    usr_folder : Path
+        The path to the user folder.
+
+    Returns
+    -------
+    List[Register]
+        The list of registers (all together).
+    """
+    logging.info("Loading registers")
+    all_regs = []
+    trajs_folder = usr_folder / "Trajectory"
+    sorted_paths = sorted(trajs_folder.iterdir())
+    for i, plt in enumerate(sorted_paths):
+        print(f"{(i + 1) / len(sorted_paths):.2%}", end="\r")
+        with open(plt, "r", encoding="utf-8") as reg:
+            registers = [parse_register(line) for line in reg.readlines()[6:]]
+            all_regs += registers
+    return all_regs
+
+
+def parse_register(line: str) -> Register:
+    """
+    Parses a register line.
+
+    Parameters
+    ----------
+    line : str
+        The line to parse.
+
+    Returns
+    -------
+    Register
+        The parsed register.
+    """
+    reg = line.strip().split(",")
     lat, lon = float(reg[0]), float(reg[1])
-    date, time = reg[5], reg[6]
-    return [lat, lon, date, time]
-        
-def range_time(dt1: dt.datetime, dt2: dt.datetime) -> int:
-    time = dt2 - dt1
-    return time.seconds
-    
-def parse_a_date(date: str) -> list:
-    d = [int(i) for i in re.split('/|-', date)]
-    return d
+    _dt = datetime.strptime(f"{reg[5]} {reg[6]}", "%Y-%m-%d %H:%M:%S")
+    return lat, lon, _dt
 
-def parse_a_time(time: str) -> list:
-    t = [int(i) for i in time.split(':')]
-    return t
-    
-def create_datetime(date: list, time: list) -> dt.datetime:
-    return dt.datetime(year=date[0], month=date[1], day=date[2], 
-                  hour=time[0], minute=time[1], second=time[2])
+
+def load_labels(labels_file: Path) -> List[LabelData]:
+    """
+    Loads the labels of a user.
+
+    Parameters
+    ----------
+    labels_file : Path
+        The path to the labels file.
+
+    Returns
+    -------
+    List[LabelData]
+        The list of labels.
+    """
+    logging.info("Loading labels")
+    with open(labels_file, "r", encoding="utf-8") as l_file:
+        labels = [parse_label(line) for line in l_file.readlines()[1:]]
+    return labels
+
+
+def parse_label(line: str) -> LabelData:
+    """
+    Parses a label line.
+
+    Parameters
+    ----------
+    line : str
+        The line to parse.
+
+    Returns
+    -------
+    LabelData
+        The parsed label.
+    """
+    items = line.split()
+    start_dt = datetime.strptime(f"{items[0]} {items[1]}", "%Y/%m/%d %H:%M:%S")
+    end_dt = datetime.strptime(f"{items[2]} {items[3]}", "%Y/%m/%d %H:%M:%S")
+    clsf = items[4]
+    return LabelData(start_dt, end_dt, clsf)
+
+
+def main():
+    """Main function. Processes all the users."""
+    dataset_folder = Path("./geolife_dataset")
+    if not dataset_folder.exists():
+        raise FileNotFoundError(f"Dataset folder not found. Path: '{dataset_folder}'")
+
+    usr_folders = list(sorted(dataset_folder.iterdir()))
+    for i, usr in enumerate(usr_folders):
+        logging.info(
+            "Processing user: %s - %s", usr.name, f"{(i + 1) / len(usr_folders):.2%}"
+        )
+        process_usr_trajs(usr)
+
+    logging.info("Saving metadata")
+    metadata_file = Path("./trajectories/metadata.json")
+    metadata_file.parent.mkdir(parents=True, exist_ok=True)
+    with open(metadata_file, "w", encoding="utf-8") as doc:
+        json.dump(metadata, doc, indent=4, ensure_ascii=False)
+    logging.info("Done")
+
 
 if __name__ == "__main__":
-    for usr in root.iterdir():
-        trajectories_usr(usr)
-        
-    with open('./trajectories/metadata.json', "w", encoding="utf-8") as doc:
-        json.dump(metadata, doc, indent=4, ensure_ascii=False)
+    main()
